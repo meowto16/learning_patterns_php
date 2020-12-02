b108ded48b2828fee0cddee65176d92e97c5cc9c
Maxim Kirshin <meowto16@gmail.com>
2020-12-03 02:33:52 +0500

    Change routes to laravel 8 way

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,35 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\FundamentalPatternsController;
+use App\Http\Controllers\CreationalPatternsController;
+use App\Http\Controllers\BehavioralPatternsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'fundamentals'], function() {
-    Route::get('property-container', 'FundamentalPatternsController@PropertyContainer');
-    Route::get('delegation', 'FundamentalPatternsController@Delegation');
-    Route::get('event-channel', 'FundamentalPatternsController@EventChannel');
-    Route::get('interface', 'FundamentalPatternsController@Interface');
+Route::group(['prefix' => 'fundamentals', 'as' => 'fundamentals.'], function() {
+    Route::get('property-container', [FundamentalPatternsController::class, 'PropertyContainer'])->name('propertyContainer');
+    Route::get('delegation', [FundamentalPatternsController::class, 'Delegation'])->name('delegation');
+    Route::get('event-channel', [FundamentalPatternsController::class, 'EventChannel'])->name('eventChannel');
+    Route::get('interface', [FundamentalPatternsController::class, 'Interface'])->name('interface');
 });
 
-Route::group(['prefix' => 'creational'], function() {
-    Route::get('abstract-factory', 'CreationalPatternsController@AbstractFactory');
-    Route::get('factory-method', 'CreationalPatternsController@FactoryMethod');
-    Route::get('static-factory', 'CreationalPatternsController@StaticFactory');
-    Route::get('simple-factory', 'CreationalPatternsController@SimpleFactory');
-    Route::get('singleton', 'CreationalPatternsController@Singleton');
+Route::group(['prefix' => 'creational', 'as' => 'creational.'], function() {
+    Route::get('abstract-factory', [CreationalPatternsController::class, 'AbstractFactory'])->name('abstractFactory');
+    Route::get('factory-method', [CreationalPatternsController::class, 'FactoryMethod'])->name('factoryMethod');
+    Route::get('static-factory', [CreationalPatternsController::class, 'StaticFactory'])->name('staticFactory');
+    Route::get('simple-factory', [CreationalPatternsController::class, 'SimpleFactory'])->name('simpleFactory');
+    Route::get('singleton', [CreationalPatternsController::class, 'Singleton'])->name('singleton');
 });
-Route::group(['prefix' => 'behavioral'], function() {
-    Route::get('strategy', 'BehavioralPatternsController@Strategy');
+Route::group(['prefix' => 'behavioral', 'as' => 'behavioral.'], function() {
+    Route::get('strategy', [BehavioralPatternsController::class, 'Strategy'])->name('strategy');
 });
