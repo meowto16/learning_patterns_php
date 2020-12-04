94b8f57d884e115b08214ad0eca011dfca82510e
Maxim Kirshin <meowto16@gmail.com>
2020-12-05 01:41:02 +0500

    Pattern builder

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,6 +21,7 @@ Route::group(['prefix' => 'creational', 'as' => 'creational.'], function() {
     Route::get('abstract-factory', [CreationalPatternsController::class, 'AbstractFactory'])->name('abstractFactory');
     Route::get('builder', [CreationalPatternsController::class, 'Builder'])->name('builder');
     Route::get('factory-method', [CreationalPatternsController::class, 'FactoryMethod'])->name('factoryMethod');
+    Route::get('lazy-initialization', [CreationalPatternsController::class, 'LazyInitialization'])->name('lazyInitialization');
     Route::get('multiton', [CreationalPatternsController::class, 'Multiton'])->name('multiton');
     Route::get('static-factory', [CreationalPatternsController::class, 'StaticFactory'])->name('staticFactory');
     Route::get('simple-factory', [CreationalPatternsController::class, 'SimpleFactory'])->name('simpleFactory');
